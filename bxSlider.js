4e102cabc0cffb03f7d611fd70004d6560bd5e4b
MinJung Choi <s2019s37@e-mirim.hs.kr>
2021-04-22 09:01:05 +0900

    first commit

---
+++ bxSlider.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider( {
+        mode: 'horizontal',// 가로 방향 수평 슬라이드
+        speed: 1500,        // 이동 속도를 설정
+        auto: true,        // 자동 실행 여부
+    });
+});
